c12ba3469c3da162f3027f2546806469ab9b9e3d
Baebin <63220297+Baebin@users.noreply.github.com>
2024-05-21 18:26:10 +0900

    Initial commit

---
+++ src/main/java/com/piebin/piebot/PieBotApplication.java
@@ -0,0 +1,13 @@
+package com.piebin.piebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PieBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piebin/piebot/PieBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piebin.piebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
