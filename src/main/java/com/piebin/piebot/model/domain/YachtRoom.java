f8e236a6d6246cf7d4912949c3caa063e39963c0
Baebin <baebine@naver.com>
2026-03-21 18:23:08 +0900

    Converter Added

--- src/main/java/com/piebin/piebot/model/domain/YachtRoom.java
+++ src/main/java/com/piebin/piebot/model/domain/YachtRoom.java
@@ -1,8 +1,12 @@
 package com.piebin.piebot.model.domain;
 
+import com.piebin.piebot.model.converter.IntegerListConverter;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -37,6 +41,14 @@ public class YachtRoom {
     @OneToOne(cascade = CascadeType.ALL)
     @Builder.Default
     private YachtScoreBoard opponentScoreBoard = new YachtScoreBoard();
+
+    @Convert(converter = IntegerListConverter.class)
+    @Builder.Default
+    private List<Integer> selectedDices = new ArrayList<>();
+
+    @Convert(converter = IntegerListConverter.class)
+    @Builder.Default
+    private List<Integer> nonSelectedDices = new ArrayList<>();
 
     private String messageId;
 }
