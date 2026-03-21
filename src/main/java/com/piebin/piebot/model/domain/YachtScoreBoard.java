6dcc479d9c5c7405864d2f3fd80ce869d108aec7
Baebin <baebine@naver.com>
2026-03-21 18:23:22 +0900

    Score Getter Added

--- src/main/java/com/piebin/piebot/model/domain/YachtScoreBoard.java
+++ src/main/java/com/piebin/piebot/model/domain/YachtScoreBoard.java
@@ -3,7 +3,7 @@ package com.piebin.piebot.model.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity
@@ -18,9 +18,12 @@ public class YachtScoreBoard {
     private Long idx;
 
     // 1 ~ 6
-    @Builder.Default
-    @ElementCollection(fetch = FetchType.EAGER)
-    private List<Integer> numbers = new ArrayList<>();
+    private Integer aces;
+    private Integer deuces;
+    private Integer threes;
+    private Integer fours;
+    private Integer fives;
+    private Integer sixes;
 
     private Integer bonus;
 
@@ -40,4 +43,26 @@ public class YachtScoreBoard {
 
     @Column(name = "yacht")
     private Integer yacht;
+
+    public List<Integer> getNumberScores() {
+        return Arrays.asList(
+                aces, deuces, threes,
+                fours, fives, sixes
+        );
+    }
+
+    public int getTotalScores() {
+        int numberScores = (aces != null ? aces : 0) + (deuces != null ? deuces : 0) + (threes != null ? threes : 0)
+                + (fours != null ? fours : 0) + (fives != null ? fives : 0);
+        return (
+                numberScores
+                        + (bonus != null ? bonus : 0)
+                        + (choice != null ? choice : 0)
+                        + (fourOfAKind != null ? fourOfAKind : 0)
+                        + (fullHouse != null ? fullHouse : 0)
+                        + (smallStraight != null ? smallStraight : 0)
+                        + (largeStraight != null ? largeStraight : 0)
+                        + (yacht != null ? yacht : 0)
+        );
+    }
 }
