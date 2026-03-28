12b138a7f7c658eb7a6ead3dd83635a8e03f80ac
Baebin <baebine@naver.com>
2026-03-28 14:49:51 +0900

    Name Null Error Fixed

--- src/main/java/com/piebin/piebot/service/impl/commands/BaboCommand.java
+++ src/main/java/com/piebin/piebot/service/impl/commands/BaboCommand.java
@@ -4,6 +4,7 @@ import com.piebin.piebot.service.PieCommand;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 @Service
 public class BaboCommand implements PieCommand {
@@ -11,7 +12,10 @@ public class BaboCommand implements PieCommand {
     public void execute(MessageReceivedEvent event) {
         User user = event.getAuthor();
         event.getGuild().retrieveMemberById(user.getId()).queue((member) -> {
-            event.getMessage().reply(member.getNickname() + " 바보.").queue();
+            String name = member.getNickname();
+            if (ObjectUtils.isEmpty(name))
+                name = member.getEffectiveName();
+            event.getMessage().reply(name + " 바보.").queue();
         });
     }
 }
